995d33ae5ba7457fb0fae88c79c5cf58faa2940f
Michael Henry <mhenry@neocrime.com>
2014-03-26 10:34:44 -0700

    making the config parsing more robust - adding error checking

--- hawkeye/__init__.py
+++ hawkeye/__init__.py
@@ -3,9 +3,27 @@
 # License, v. 2.0. If a copy of the MPL was not distributed with this
 # file, You can obtain one at http://mozilla.org/MPL/2.0/.
 
+import sys
+import os
 
-def parse_config(path):
+def _open_config(path):
+    try:
+        return file(path,"r").read()
+    except:
+        sys.exit("Error reading configuration file " + str(path))
+
+def _parse_config(data):
+    try:
+        return eval(data)
+    except:
+        sys.exit("Error parsing configuration file " + str(path) +
+            ".\nTry executing 'python " + str(path) + "' for more information.")
+
+def get_config(path):
     """ simple python config file reader """
-    return eval(file(path,"r").read())
-
+    if os.path.exists(path):
+        data = _open_config(path)
+        return _parse_config(data)
+    else:
+        sys.exit("Configuration file does not exist: "+str(path))
 
